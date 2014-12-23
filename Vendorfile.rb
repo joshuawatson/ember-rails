53ad192759d7e094bd9401b33c4a247305302176
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-12-23 22:50:43 +0900

    Update vendor file urls
    They are moved to ember-cli organizations.

--- Vendorfile.rb
+++ Vendorfile.rb
@@ -1,7 +1,7 @@
 require './lib/ember/rails/version'
 
 folder 'vendor/assets/javascripts/ember-rails' do
-  file 'loader.js', "https://raw.githubusercontent.com/stefanpenner/loader.js/#{Ember::Rails::EMBER_LOADER_VERSION}/loader.js"
-  file 'ember-resolver.js', "https://raw.githubusercontent.com/stefanpenner/ember-resolver/v#{Ember::Rails::EMBER_RESOLVER_VERSION}/dist/ember-resolver.js"
-  file 'ember-load-initializers.js', "https://raw.githubusercontent.com/stefanpenner/ember-load-initializers/v#{Ember::Rails::EMBER_LOAD_INITIALIZERS_VERSION}/ember-load-initializers.js"
+  file 'loader.js', "https://raw.githubusercontent.com/ember-cli/loader.js/#{Ember::Rails::EMBER_LOADER_VERSION}/loader.js"
+  file 'ember-resolver.js', "https://raw.githubusercontent.com/ember-cli/ember-resolver/v#{Ember::Rails::EMBER_RESOLVER_VERSION}/dist/ember-resolver.js"
+  file 'ember-load-initializers.js', "https://raw.githubusercontent.com/ember-cli/ember-load-initializers/v#{Ember::Rails::EMBER_LOAD_INITIALIZERS_VERSION}/ember-load-initializers.js"
 end
